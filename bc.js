9c5d0f2019791bc801705540afc18f75be8d454d
Dixsas <55358921+Dixsas@users.noreply.github.com>
2019-10-17 12:59:20 +0300

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Special Arab`,"https://www.twitch.tv/m3lm_pc")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NTY1MTg3MTQ5MDU0NDc2Mjk5.XZXYcA.YzP4qv7rLZXPDZOnjeTlqLtxSlo");
